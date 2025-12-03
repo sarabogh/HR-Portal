ae744b7a80f9a9e2abbbaaf7bc5170da3b78e191
sarabogh <sarabogh22@gmail.com>
2025-12-02 20:56:06 -0700

    Change Router import from BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Login from "./Pages/Login/Login";
 import HRPolicy from "./Pages/HRPolicy/HRPolicy";
